49ac1a32ef955611ef504c7f26e388ed4a8f2d63
FabianAlvarez1 <7abianalvarez@gmail.com>
2021-10-31 05:19:52 -0600

    Agregando CSS y JAvaScript a la página

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// File test
+
+function getName(){
+
+    let name = prompt("Your name:");
+    console.log(name);
+}
+
+// array with objects
+let articles = [
+
+    { name: "Laptop", price: 22000, weight: "1.3kg" },
+    { name: "Mouse", price: 400, weight: "0.1kg" },
+    { name: "Keyboar", price: 1400, weight: "0.5kg" },
+    { name: "Speakers", price: 22000, weight: "1.3kg" },
+]
+
+let array = ["One", "Two", "Three","Four", "Six"];
+let number = 1;
+let time = Date.now();
+
+getName();
+document.write("Hello world");
+
